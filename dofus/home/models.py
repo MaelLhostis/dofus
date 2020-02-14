93cd06f98b5bc3c4b3f3890a71358aaede82d12f
kevin <kevin.houd@gmail.com>
2020-02-14 22:53:43 +0100

    Ajout des classe

--- dofus/home/models.py
+++ dofus/home/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    mail = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+class Server(models.Model):
+    name = models.CharField(max_length=30)
+
+class Alliance(models.Model):
+    idServer = models.ForeignKey(Server, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    descrip = models.CharField(max_length=550)
+    recrutement = models.BooleanField()
+    lvlMinRecrutement = models.IntegerField()
+
+class Guild(models.Model):
+    idServer = models.ForeignKey(Server, on_delete=models.CASCADE)
+    idAlliance = models.ForeignKey(Alliance, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    descrip = models.CharField(max_length=550)
+    recrutement = models.BooleanField()
+    lvlMinRecrutement = models.IntegerField()
+
+class Perso(models.Model):
+    idServeur = models.ForeignKey(Server, on_delete=models.CASCADE)
+    idGuild = models.ForeignKey(Guild, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    level = models.IntegerField()
+    classe = models.CharField(max_length=15)
+    descrip = models.CharField(max_length=550)
+    pvpm = models.CharField(max_length=3)
+    dbook = models.CharField(max_length=40)
+
+
+
