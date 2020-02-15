b85d910a028c1ce6e23c7ec2e7b7a9549afeb0b3
kevin <kevin.houd@gmail.com>
2020-02-15 14:47:43 +0100

    newscript

--- dofus/components/management/commands/generateDatabase.py
+++ dofus/components/management/commands/generateDatabase.py
@@ -4,18 +4,15 @@ from  home.models import Server
 from  home.models import Alliance
 from  home.models import Guild
 from  home.models import Player
-
+from  home.models import DofusClass
+from  home.models import Pvpm
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        s = Server()
-        s.name = "s1"
-        s.save()
-
         a = Alliance()
-        a.server = s 
+        a.server = Server.JULITH
         a.name = "A1" 
         a.description = "test1"
         a.recrutement = True
@@ -24,418 +21,156 @@ class Command(BaseCommand):
         
         
         g = Guild()
-        g.server = s
+        g.server = Server.PANDORE
         g.alliance = a
         g.name = "g1"
         g.description = "d1"
         g.recrutement = True
         g.lvlMinRecrutement = 80
+        g.pvpm = Pvpm.PVM
         g.save()
 
         p = Player()
-        p.server = s
+        p.server = Server.ILYZAELLE
         p.guild = g
         p.name = "p1"
         p.level = 80
-        p.classe = "class1"
+        p.classe = DofusClass.FECA
         p.description = "desc1"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVM
         p.dbook = "toto"
         p.save()
 
         p = Player()
-        p.server = s
+        p.server = Server.AGRIDE
         p.guild = g
         p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
+        p.level = 160
+        p.classe = DofusClass.ZOBAL
         p.description = "desc2"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVP
         p.dbook = "toto3"
         p.save()
 
 
         p = Player()
-        p.server = s
+        p.server = Server.AGRIDE
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 40
+        p.classe = DofusClass.ENUTROF
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVM
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.OMBRE
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 200
+        p.classe = DofusClass.FECA
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVPM
+        p.dbook = "toto1"
+        p.save()
+
+
+        p = Player()
+        p.server = Server.OMBRE
+        p.guild = g
+        p.name = "p3"
+        p.level = 120
+        p.classe = DofusClass.SACRIEUR
+        p.description = "desc3"
+        p.pvpm = Pvpm.PVM
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.BRUMEN
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 40
+        p.classe = DofusClass.HUPPERMAGE
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVP
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.JULITH
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 186
+        p.classe = DofusClass.XELOR
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVP
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.NIDAS
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 186
+        p.classe = DofusClass.SADIDA
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVPM
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.JULITH
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 186
+        p.classe = DofusClass.XELOR
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVP
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.JULITH
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 75
+        p.classe = DofusClass.XELOR
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVP
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.JULITH
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 75
+        p.classe = DofusClass.ZOBAL
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVM
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.OMBRE
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 75
+        p.classe = DofusClass.OUGINAK
         p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVPM
         p.dbook = "toto1"
         p.save()
 
         p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
+        p.server = Server.MERKATOR
         p.guild = g
         p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
+        p.level = 75
+        p.classe = DofusClass.ZOBAL
         p.description = "desc3"
-        p.pvpm = "pvp"
-        p.dbook = "toto1"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
-        p.description = "desc3"
-        p.pvpm = "pvp"
-        p.dbook = "toto1"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p1"
-        p.level = 80
-        p.classe = "class1"
-        p.description = "desc1"
-        p.pvpm = "pvp"
-        p.dbook = "toto"
-        p.save()
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p2"
-        p.level = 80
-        p.classe = "class2"
-        p.description = "desc2"
-        p.pvpm = "pvp"
-        p.dbook = "toto3"
-        p.save()
-
-
-        p = Player()
-        p.server = s
-        p.guild = g
-        p.name = "p3"
-        p.level = 80
-        p.classe = "class3"
-        p.description = "desc3"
-        p.pvpm = "pvp"
+        p.pvpm = Pvpm.PVPM
         p.dbook = "toto1"
         p.save()
--- dofus/home/models.py
+++ dofus/home/models.py
@@ -1,37 +1,18 @@
 from django.db import models
 
-class User(models.Model):
-    mail = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-
 class Server(models.Model):
-    name = models.CharField(max_length=30)
-
-class Alliance(models.Model):
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=550)
-    recrutement = models.BooleanField()
-    lvlMinRecrutement = models.IntegerField()
-
-class Guild(models.Model):
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)
-    alliance = models.ForeignKey(Alliance, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=550)
-    recrutement = models.BooleanField()
-    lvlMinRecrutement = models.IntegerField()
-
-class Player(models.Model):
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    level = models.IntegerField()
-    DofusClasse = models.CharField(max_length=15)
-    description = models.CharField(max_length=550)
-    pvpm = models.CharField(max_length=3)
-    dbook = models.CharField(max_length=40)
+    AGRIDE = "Agride"
+    ILYZAELLE = "Illyzaelle"
+    USH = "Ush"
+    MERIANA = "Mériana"
+    PANDORE = "Pandore"
+    NIDAS = "Nidas"
+    MERKATOR = "Merkator"
+    BRUMEN = "Brumen"
+    JULITH = "Julith"
+    FURYE = "Furye"
+    OTOMUSTAN = "Oto Mustam"
+    OMBRE = "Ombre"
 
 class DofusClass(models.Model):
     CRA = "Cra"
@@ -53,16 +34,39 @@ class DofusClass(models.Model):
     HUPPERMAGE = "Hupermage"
     OUGINAK = "Ouginak"
 
-class Server(models.Model):
-    AGRIDE = "Agride"
-    ILYZAELLE = "Illyzaelle"
-    USH = "Ush"
-    MERIANA = "Mériana"
-    PANDORE = "Pandore"
-    NIDAS = "Nidas"
-    MERKATOR = "Merkator"
-    BRUMEN = "Brumen"
-    JULITH = "Julith"
-    FURYE = "Furye"
-    OTOMUSTAN = "Oto Mustam"
-    OMBRE = "Ombre"
+class Pvpm(models.Model):
+    PVP = "PVP"
+    PVM = "PVM"
+    PVPM = "PVP/PVM"
+
+class User(models.Model):
+    mail = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+
+class Alliance(models.Model):
+    server = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=550)
+    recrutement = models.BooleanField()
+    lvlMinRecrutement = models.IntegerField()
+
+class Guild(models.Model):
+    server =  models.CharField(max_length=30)
+    alliance = models.ForeignKey(Alliance, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=550)
+    recrutement = models.BooleanField()
+    lvlMinRecrutement = models.IntegerField()
+    pvpm = models.CharField(max_length=50)
+
+class Player(models.Model):
+    server =  models.CharField(max_length=30)
+    guild = models.ForeignKey(Guild, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    level = models.IntegerField()
+    DofusClasse = models.CharField(max_length=15)
+    description = models.CharField(max_length=550)
+    pvpm = models.CharField(max_length=50)
+    dbook = models.CharField(max_length=40)
