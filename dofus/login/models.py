be0069bcb08184d366ff08efd1200ce8cb8e273b
MaelLhostis <mael.lhostis@outlook.fr>
2020-02-16 15:00:34 +0100

    design

--- dofus/login/models.py
+++ dofus/login/models.py
@@ -4,11 +4,10 @@ from django import forms
 
 class ConnectForm(forms.Form):
 
-    username = forms.CharField(label='username')
-    password = forms.CharField(label='password')
-
+    username = forms.CharField(label='username', widget=forms.TextInput(attrs={'placeholder': 'Entrez un username'}))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'placeholder': 'Entrez un password'}))
 class SubscribeForm(forms.Form):
 
-    mail = forms.CharField(label='mail')
-    username = forms.CharField(label='username')
-    password = forms.CharField(label='password')
+    mail = forms.CharField(label='mail', widget=forms.TextInput(attrs={'placeholder': 'Entrez un Email'}))
+    username = forms.CharField(label='username', widget=forms.TextInput(attrs={'placeholder': 'Entrez un username'}))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'placeholder': 'Entrez un password'}))
