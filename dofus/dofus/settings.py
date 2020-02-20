b70991c12a2708cc6fc53a840d0d3969bf5e211e
MaelLhostis <mael.lhostis@outlook.fr>
2020-02-20 23:17:17 +0100

    settings

--- dofus/dofus/settings.py
+++ dofus/dofus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.auth',
+    'django.contrib.sites',
     'home',
     'search',
     'components',
